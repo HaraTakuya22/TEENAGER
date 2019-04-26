721eb74e88b421e997a2dd5440e4a34059f7df27
HaraTakuya <taku-uver.rc-youtu@ezweb.ne.jp>
2019-04-26 10:38:16 +0900

    Add

--- Source/GameScene.cpp
+++ Source/GameScene.cpp
@@ -1,4 +1,5 @@
 #include <DxLib.h>
+#include "Scene.h"
 #include "GameScene.h"
 #include "Map.h"
 
@@ -16,6 +17,11 @@ GameScene::~GameScene()
 unique_Base GameScene::Update(unique_Base own, const Controller & Controller)
 {
 	
+	// objList‚Ì±¯ÌßÃÞ°Ä
+	for (auto itr = objlist->begin(); itr != objlist->end(); itr++)
+	{
+		(*itr)->Update(Controller, objlist);
+	}
 
 	ClsDrawScreen();
 	Draw();
@@ -27,13 +33,26 @@ unique_Base GameScene::Update(unique_Base own, const Controller & Controller)
 
 int GameScene::Init(void)
 {
+	if (!objlist)
+	{
+		objlist = std::make_shared<Shared_ObjList>();
+	}
 	lpMap.MapInit();
 	return 0;
 }
 
 void GameScene::Draw(void)
 {
+	auto Scr = lpScene.GetScrSize();
+
 	DrawFormatString(600, 0, 0xffff00, "Main");
+	lpMap.CreateIndividualsDisplay();
+	lpMap.IndividualsDraw(objlist);
+
+	for (auto itr = objlist->begin(); itr != objlist->end(); itr++)
+	{
+		(*itr)->Draw();
+	}
 	lpMap.MapDraw();
-	lpMap.IndividualsDraw();
+
 }
